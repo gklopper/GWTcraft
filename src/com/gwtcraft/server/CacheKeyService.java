12c617ff011457307383129d4ed34e2447d90af6
Grant Klopper <grant.klopper@gmail.com>
2010-01-10 10:49:22 +0000

    Grant - minor styling

--- src/com/gwtcraft/server/CacheKeyService.java
+++ src/com/gwtcraft/server/CacheKeyService.java
@@ -2,7 +2,7 @@ package com.gwtcraft.server;
 
 public class CacheKeyService {
 	
-	private static final String version = "a1_";
+	private static final String version = "a2_";
 	
 	public static String key(String key) {
 		return version + key;
